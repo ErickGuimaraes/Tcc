90e6160e2b4b2df2dabd40a15503a3afe5ef2cd9
BrLocha94 <brunolocha2@gmail.com>
2020-11-09 20:42:42 -0300

    First connection atempt from unity sucess

---
+++ Unity/Source/Assets/ConnectionTest/Scripts/UiManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    public InputField inputField;
+
+    public static UiManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
+
+    public void ConnectToServer()
+    {
+        inputField.interactable = false;
+        Client.instance.ConnectToServer();
+    }
+}
